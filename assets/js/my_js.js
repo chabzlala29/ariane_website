b858de6090a8d28454dd5888c6650497da4bd0f6
Charles Darwin D.C. Pobre <cdpobre@yahoo.com>
2013-09-21 21:23:32 -0700

    first commit

---
+++ assets/js/my_js.js
@@ -0,0 +1,18 @@
+$(function(){
+	$('#navbar-main .gallery-link').bind('click',function(){
+   		$("html, body").animate({ scrollTop: $('.gallery').offset().top }, 500);
+	});
+	$('#navbar-main .about-link').bind('click',function(){
+   		$("html, body").animate({ scrollTop: $('.about-me').offset().top }, 500);
+	});
+	$('#navbar-main .favorites-link').bind('click',function(){
+   		$("html, body").animate({ scrollTop: $('.favorites-container').offset().top }, 500);
+	});
+
+	$('.back-to-top').click(function(){
+		$("html, body").animate({ scrollTop: 0 }, 500);
+	});
+
+
+	$('[data-toggle="tooltip"]').tooltip();
+})
